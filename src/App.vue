afec670ff09bb9511daaa3ebeca64d6b3469d5cf
sCar3Cr0w25 <wretchedegg8@gmail.com>
2022-07-14 15:53:28 +0900

    todo homework

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div id="app">
+    <todo-app></todo-app>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import TodoApp from './components/TodoApp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    TodoApp
   }
 }
 </script>
---
+++ src/components/TodoApp.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <!-- Heading -->
+    <h2>Todo List</h2>
+
+    <!-- Input -->
+    <div class="d-flex mt-5">
+      <input type="text" v-model="task" placeholder="Enter task" class="w-100 form-control" />
+      <button @click="submit">
+        Add
+      </button>
+      <div v-for="task in tasks" :key="task.id" >
+        <input type=checkbox  @click="toggleDone(task)" >
+        <span :class="{ done: task.done }">{{task.name }}</span>
+      </div>
+      <button @click="removeAll()">clear</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoApp",
+  data() {
+    return {
+      task: "",
+      tasks: [
+
+      ],
+    };
+  },
+  methods: {
+    submit() {
+      if (this.task.length === 0) return;
+        this.tasks.push({
+          name: this.task,
+          id: this.tasks.length + 1,
+          done: false,
+        });
+      this.task = "";
+    },
+    toggleDone(task) {
+      task.done = !task.done;
+    },
+    removeAll(){
+      this.tasks = []
+    },
+  },
+};
+</script>
+
+<style>
+.done {
+  text-decoration: line-through;
+}
+</style>
